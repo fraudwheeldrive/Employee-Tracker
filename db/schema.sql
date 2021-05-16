e29e5dbb9baf452ba7a41fc07ac7a5be71b613fa
[Andrew Core] <[adcore89@gmail.com]>
2021-05-16 11:48:55 -0400

    sql seeds, schena, db added, connection connected toi app,js

--- db/schema.sql
+++ db/schema.sql
@@ -3,24 +3,27 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS position;
 
 
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL, 
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
-);
-
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    department_name VARCHAR(30)
 );
 
 CREATE TABLE position (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL (5,2),
-    department_id INTEGER 
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INTEGER,
+    position_id INTEGER,
+    CONSTRAINT fk_position FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL
 );
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,22 @@
-INSERT INTO employee (first_name, last_name)
+
+INSERT INTO department (department_name)
+VALUES 
+('Automation'),
+('IT'),
+('Inside Sales'),
+('Warehouse');
+
+INSERT INTO position (title, salary, department_id)
+VALUES 
+('AVP' , 300.5),
+('Manager' , 90.2),
+('Team Lead' , 60.7),
+('Associate' , 42.3);
+
+INSERT INTO employee (first_name, last_name, manager_id, position_id)
 VALUES 
 ('Dale' , 'Dog'),
 ('Andrew' , 'Core'),
 ('Sam', 'Cat'),
 ('Pip', 'Kitty');
+
