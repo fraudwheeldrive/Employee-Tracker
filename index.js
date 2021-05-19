95e9e06a98263aaea9cf2108895b2e9c878c652e
[Andrew Core] <[adcore89@gmail.com]>
2021-05-18 21:39:05 -0400

    View commands completed, add dep completed

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-
-//required dependancies 
-const inquirer = require('inquirer');
-const mysql = require('mysql');
-const CTable = require('console.table');
-
+//required dependancies
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const CTable = require("console.table");
 
 //const db = require('./db/connection')
 
-const db = mysql.createConnection (
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'Raptor10!',
-        database: 'employee_tracker'
-    },
-    console.log('connected to the Employee Tracker database.')
-
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Raptor10!",
+    database: "employee_tracker",
+  },
+  console.log("connected to the Employee Tracker database.")
 );
 
-db.connect(function(err){
-    if (err) throw err;
-    promptUser();
-})
+db.connect(function (err) {
+  if (err) throw err;
+  promptUser();
+});
 
-// start inquirer 
+// start inquirer
 
- function promptUser() {
-    console.log(`
+function promptUser() {
+  console.log(`
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     *******EMPLOYEE TRACKER*******
@@ -34,111 +31,198 @@ db.connect(function(err){
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     `);
 
-    return inquirer.prompt([
-        { 
-            type: "list",
-            name: "startTracker",
-            message: "What would you like to do?",
-            choices: [
-                "view all departments",
-                "view all roles",
-                "view all employees",
-                "add a department",
-                "add a role",
-                "add an employee",
-                "update an employee role"
-            ]
-        }
-    ])
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "startTracker",
+          message: "What would you like to do?",
+          choices: [
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add a department",
+            "add a role",
+            "add an employee",
+            "update an employee role",
+          ],
+        },
+      ])
 
- // need to set up what to do with the selected answer 
- 
-.then(promptAnswer => {
+      // need to set up what to do with the selected answer
 
-    switch(promptAnswer.startTracker){
-
-        case "view all departments":no
+      .then((promptAnswer) => {
+        switch (promptAnswer.startTracker) {
+          case "view all departments":
             allDep();
             break;
 
-        case "view all roles":
+          case "view all roles":
             allRoles();
             break;
-            
-        case "view all employees":
+
+          case "view all employees":
             allEmp();
             break;
-        
-        case "add a department":
+
+          case "add a department":
             addDep();
-            break;   
+            break;
 
-        case "add a role":
-             addRoll();
-             break;
-             
-        case "add an employee":
-             addEmp();
-             break;
-        
-        case  "update an employee role":
-             empUpdate();
-             break;    
+          case "add a role":
+            addRole();
+            break;
 
-        default:
-            console.log= ("program has quit");
-            process.exit();     
-       };
-    });
-};
+          case "add an employee":
+            addEmp();
+            break;
 
+          case "update an employee role":
+            empUpdate();
+            break;
 
-// //display all departments  
+          default:
+            console.log = "program has quit";
+            process.exit();
+        }
+      })
+  );
+}
+
+// //display all departments
 
 // SELECT * from department;
-function allDep(){
-    const sql = `SELECT * from department`;
-    db.query(query, function(err, res){
-        if (err) throw err;
-        console.table('employee', res);
-        promptUser();
-    })
-};
-    
-   
+function allDep() {
+  const sql = `SELECT * from department`;
+  db.query(sql, function (err, res) {
+    if (err) throw err;
+    console.table("employee", res);
+    promptUser();
+  });
+}
 
-//"view all roles", 
-
+//"view all roles",
 // SELECT * from ROLES;
+function allRoles() {
+  const sql = `SELECT * from roles`;
+  db.query(sql, function (err, res) {
+    if (err) throw err;
+    console.table("roles", res);
+    promptUser();
+  });
+}
 
-//"view all employees", 
-// THEN I am presented with a formatted table showing employee data, 
-// including employee ids, 
-// first names, last names, 
-// job titles, departments, salaries, and 
-// managers that the employees report to
-// SELECT employee.ID, employee.first_name, employee.last_name, roles.title, roles.salary, roles.department_id
-//from employee
-//
+//"view all employees",
 // SELECT * FROM employee
 // LEFT JOIN roles ON employee.roles_id = roles.id;
 
+function allEmp() {
+  const sql = `SELECT * FROM employee LEFT JOIN roles ON employee.roles_id = roles.id`;
+  db.query(sql, function (err, res) {
+    if (err) throw err;
+    console.table("roles", res);
+    promptUser();
+  });
+}
 
 //"add a department",
 // INSERT INTO department (name)
 // Values('');
 
+function addDep() {
+  inquirer
+    .prompt([
+      {
+        name: "newDep",
+        message: "what is the new Departments Name",
+        validate: (newDepInput) => {
+          if (newDepInput) {
+            return true;
+          } else {
+            console.log("please enter a New Department Name");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answer) => {
+      console.log(answer.newDep);
+      const newDepartmentName = answer.newDep;
+      const sql = `INSERT INTO department (name) VALUES (?)`;
+      db.query(sql, [newDepartmentName], function (err, res) {
+        if (err) throw err;
+        console.table("department", res);
+        promptUser();
+      });
+    });
+}
+
+function addRole() {
+  inquirer.prompt([
+    {
+      name: "newTitle",
+      message: "what is the new Title Name ",
+      validate: (newTitleInput) => {
+        if (newTotleInput) {
+          return true;
+        } else {
+          console.log("please enter a New Title");
+          return false;
+        }
+      },
+    },
+    {
+      name: "newSalary",
+      message: "what is the new Roles Salary in the format of XXXXX.XX",
+      validate: (newSalaryInput) => {
+        if (newSalaryInput) {
+          return true;
+        } else {
+          console.log("please enter a Salary");
+          return false;
+        }
+      },
+    },
+    {
+      name: "newDepId",
+      message: "please specify a new Department ID (numbers only)",
+      validate: (newDepID) => {
+        if (newDepID) {
+          return true;
+        } else {
+          console.log("please enter a new departmen ID ");
+          return false;
+        }
+      },
+    },
+  ])
+//   .then((answer) => {
+//     console.log(answer.newTitle, answer.newSalary, answer.newDepID);
+//     const addARole = answer.newTitle, answer.newSalary, answer.newDepID;
+//     const sql = `INSERT INTO rolls (title, salary, department_id) VALUES (?,?,?)`;
+//     db.query(sql, [addARole], function (err, res) {
+//       if (err) throw err;
+//       console.table("roles", res);
+//       promptUser();
+//     });
+//   });
+
+
+
+
+}
 //"add a role",
 // INSERT INTO roles (title, salary, department_id)
-// VALUES 
+// VALUES
 // ('' ,  ,  ),
 
 //"add an employee",
 // INSERT INTO employee (first_name, last_name, roles_id , manager_id)
-// VALUES 
+// VALUES
 // ('' , '',  , ),
 
 // //"update an employee role"
 // update employee
-//     -> set roles_id =  
+//     -> set roles_id =
 //     -> where ID = ;
